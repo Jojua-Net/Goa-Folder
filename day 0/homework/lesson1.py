52a5c346c8d4094df513d197321a992ced296ecf
Jojua-Net <jojua.vaxtang@gmail.com>
2025-01-17 21:21:03 +0400

    first commit

---
+++ day 0/homework/lesson1.py
@@ -0,0 +1,57 @@
+#print("Vaxtang Jojua")
+
+#print(5)
+#print("5")
+
+#print(5 + int("5"))
+
+
+#print("Vaxtang Jojua " + "5" + " wlis")
+
+
+from turtle import *
+
+# we wanto to paint a house
+
+
+#step 1: draw a square
+speed(30)
+width(7)
+color("purple")
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+#end of square
+
+#drawing a door
+forward(70)
+color("yellow")
+left(90)
+forward(120)
+right(90)
+forward(60)
+right(90)
+forward(120)
+
+
+penup()
+goto(200, 200)
+pendown()
+
+color("red")
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+
+exitonclick()
