33db1896238f4a521a201a276ec56a1e961ff9ae
Jojua-Net <jojua.vaxtang@gmail.com>
2025-01-18 02:11:41 +0400

    axali 1

---
+++ day 1/classwork/lesson1.py
@@ -0,0 +1,37 @@
+name = "Vaxo"
+#name არის ცვლადი
+# = რის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "vaxo" არის ცვლადისთვის მნიშვნელობა
+
+surname = "Jojua"
+
+# print(name, surname)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "Vaxo" #ეს არის str (string) ტიპის ცვლადი
+age = 15 # ეს არის int ( integer ) მთელი რიცხვი
+height = 183.5 #ეს არის float ტიპის ცვლადი ( ათწილადი )
+#booLean (bool) ტიპის ცვლადი
+
+
+knows_programming = True # True ან False
+is_ugly = False #snakecase( უბრალოდ წერის სტილი სტანდარტულად)
+
+isUgly = False # ჯავასკრიპტული camelcase
+
+# print(name + " " + surname)
+
+# სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+
+
+
+print(type(age))
+print(type(name))
+print(type(surname))
+print(type(height))
+print(type(knows_programming))
+
+
+
+
+print(name + str(age))
---
+++ day 1/homework/lesson1.py
@@ -0,0 +1,10 @@
+
+
+name_info = "Hello, my name is Vaxo"
+surname = " Jojua. "
+age = "I am " + str(15) + " years old, "
+height = str(183.5) + " cm tall"
+more_text = " And I started studying in Goa. I really like the lessos taught in Goa <3"
+
+
+print(name_info + surname + age + height + more_text)
