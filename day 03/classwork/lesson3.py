3ed1b64bff48f4f588796f8ed1107017cb68b098
Jojua-Net <jojua.vaxtang@gmail.com>
2025-02-17 00:33:16 +0400

    day 3

---
+++ day 03/classwork/lesson3.py
@@ -0,0 +1,59 @@
+# string, boolean, float, integer, None
+
+
+# input
+# variable
+# print
+
+
+# num1 = int(input("enter num1: "))
+# num2 = int(input("enter num2: "))
+# num3 = int(input("enter num3: "))
+# print(num1+num2+num3)
+
+# username = input("Enter Name: ")
+# print(username)
+
+
+# num1 = int(input("Enter Num1: "))
+# num2 = int(input("Enter Num2: "))
+
+# print(num1>num2)
+
+
+
+
+# my_list = ["Vaxo", "Jojua", 15, 1.86]
+
+
+# for x in my_list:
+#     print(x)
+
+
+
+
+
+# usertext = input("Enter Text: ")
+
+# n = int(input("Enter Num: "))
+
+# print(usertext * n)
+
+
+
+
+
+# num1 = input("Enter Num1: ")
+# num2 = input("Enter Num2: ")
+
+# num3 = num1 + num2
+
+# print(str(num3))
+
+
+
+
+                    
+
+
+
