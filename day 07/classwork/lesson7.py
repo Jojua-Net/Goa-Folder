d0cfda901f0b25571933e9e950a1a08943d2fb21
Jojua-Net <jojua.vaxtang@gmail.com>
2025-03-03 02:23:17 +0400

    day 7

---
+++ day 07/classwork/lesson7.py
@@ -0,0 +1,83 @@
+
+# (A && !B) || (B && !A) --> True
+
+# if (10>5 and not 5>10) or (5>10 and not 8>7):
+#     print("yes")
+
+
+
+# (B && C) && (A || B) -- False
+
+# if (False and 10>11) and (True or 90<50):
+#     print("yes")
+
+
+# if (True and not False) or (False and not True) or (False and not False):
+#     print("yes")
+
+
+
+
+
+# davaleba 1
+
+# user_string = input("Enter String: ")
+
+# print(user_string)
+
+# user_int = int(input("Enter int: "))
+
+
+# print(user_string[user_int])
+
+
+
+
+
+# davaleba 2
+
+# for i in range(100, 0, -1):
+#     print(i)
+
+
+
+# davaleba 3
+
+# for i in range(1,100):
+#     if i % 2 == 1:
+#         print(i)
+
+
+
+# davaleba 4
+
+# for i in range(250,500, 10):
+#     print(i)
+
+
+# davaleba 5
+
+# user_string = input("Enter string: ")
+
+
+
+# if user_string[0] == "_":
+#     print(True)
+# else:
+#     print(False)
+
+
+# davaleba 6
+
+# user_integer = int(input("Enter Int: "))
+
+
+# if user_integer % 2 == 0:
+#     for i in range(0, 10):
+#         print("yes")
+# else:
+#     print("No")
+
+
+
+
