ece5912dc9a3078362dc0f32a46872f42b051648
Jojua-Net <jojua.vaxtang@gmail.com>
2025-05-25 20:05:08 +0400

    update

---
+++ day 029/homework/hw5.py
@@ -0,0 +1,7 @@
+# 5.შეისწავლეთ chr და ord Python-ში, რესურსები:
+# 
+# chr(): https://www.w3schools.com/python/ref_func_chr.asp
+# ord(): https://www.w3schools.com/python/ref_func_ord.asp
+
+
+print(chr(122) == "z" and 122 == ord("z"))
