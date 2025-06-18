0144d4cd685450111bde53f5abc560eebdfd1b65
Jojua-Net <jojua.vaxtang@gmail.com>
2025-06-18 13:51:27 +0400

    update

---
+++ day 035/homework/hw3.py
@@ -0,0 +1,11 @@
+# 3. დაწერეთ ამ ლოგიკით კოდი Python-ში:
+
+
+age = int(input("Enter your age: "))
+
+if age > 0 and age < 18:
+    print("შენ არ ხარ სრულწლოვანი")
+elif age < 0:
+    print("დაბადებულიც არ ხარ ეს არასწორი პასუხია :დ")
+else:
+    print("შენ სრულწლოვანი ხარ")
