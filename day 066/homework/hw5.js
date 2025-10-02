34195b77c919d54638896b509b60386ebfd87f32
Jojua-Net <jojua.vaxtang@gmail.com>
2025-10-02 16:53:41 +0400

    update

---
+++ day 066/homework/hw5.js
@@ -0,0 +1,14 @@
+
+
+
+// 5) შექმენით arr, 
+// გადაუარეთ ამ arr-ს 
+// for loop-ით და დაბეჭდეთ თითოეული ელემენტი
+
+
+const arr = ["hello", "world", "gamarjoba", "samyaro"]
+
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+}
---
+++ day 066/homework/hw6.js
@@ -0,0 +1,12 @@
+
+
+
+// 6) მასივში ['a', 'b', 'c', 'd'] 
+// თითოეული ელემენტი გადააქციო დიდ ასოებად და დაბეჭდო.
+
+
+const arr = ['a', 'b', 'c', 'd'] 
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i].toUpperCase())
+}
